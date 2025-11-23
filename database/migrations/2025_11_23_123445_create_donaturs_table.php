019cf80ea220685b1a94e0515e4c3e59914d1f13
Muhammad Raja <122259770+Javessboyy@users.noreply.github.com>
2025-11-23 22:36:02 +0700

    First Committ

---
+++ database/migrations/2025_11_23_123445_create_donaturs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donaturs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->unsignedBigInteger('fundraising_id');
+            $table->unsignedBigInteger('total_amount');
+            $table->string('notes');
+            $table->string('proof');
+            $table->string('is_paid');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donaturs');
+    }
+};
