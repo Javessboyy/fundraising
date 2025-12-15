465bfc683ac16e368862ef5cb0d8627816775926
Muhammad Raja <122259770+Javessboyy@users.noreply.github.com>
2025-12-15 23:57:03 +0700

    add: feat fundraising
    nambahin code

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
@@ -28,9 +32,25 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        DB::transaction(function() use ($request){
+
+            $validated = $request->validated();
+
+            if($request->hasFile('icon')){
+                $iconPath = $request->file('icon')->store('icons','public');
+                $validated['icon'] = $iconPath;
+            }else{
+                $iconPath = 'images/icon-category-default.png';
+            }
+
+            $validated['slug'] = Str::slug($validated['name']);
+
+            $category = Category::create($validated);
+        });
+
+        return redirect()->route('admin.categories.index');
     }
 
     /**
@@ -52,9 +72,25 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        DB::transaction(function() use ($request, $category){
+
+            $validated = $request->validated();
+
+            if($request->hasFile('icon')){
+                $iconPath = $request->file('icon')->store('icons','public');
+                $validated['icon'] = $iconPath;
+            }else{
+                $iconPath = 'images/icon-category-default.png';
+            }
+
+            $validated['slug'] = Str::slug($validated['name']);
+
+            $category->update($validated);
+        });
+
+        return redirect()->route('admin.categories.index');
     }
 
     /**
@@ -62,6 +98,16 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        DB::beginTransaction();
+
+        try {
+            $category->delete();
+            DB::commit();
+            return redirect()->route('admin.categories.index');
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            return redirect()->route('admin.categories.index');
+        }
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,7 +33,7 @@
                     </x-nav-link>
                 @endrole
                 
-                @role('owner|fundraisers')
+                @role('owner|fundraiser')
                  <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('admin.fundraisings.index')" :active="request()->routeIs('admin.fundraisings.index')">
                         {{ __('Fundraisings') }}
@@ -60,7 +60,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div class="flex flex-row items-center gap-x-5"><img class="w-[10px] h-[10px]" src="{{ Storage::url(Auth::user()->avatar)}}"/>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
